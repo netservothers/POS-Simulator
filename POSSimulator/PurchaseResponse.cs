5513717cd13b3d7cf144308753a8eb7e42be052c
Aruna.V <aruna.v@smnetserv.com>
2021-01-07 18:01:31 +0530

    Void Response
    1. Removed unnecesary Response codes.

--- POSSimulator/PurchaseResponse.cs
+++ POSSimulator/PurchaseResponse.cs
@@ -8,16 +8,8 @@ namespace POSSimulator
     public sealed class PurchaseResponse : POSResponseBase
 	{
 
-		/// <summary>
-		/// Constructs a new message instance from the list of pre-decoded string values received from the pinpad.
-		/// </summary>
-		/// <param name="fieldValues">The list of values returned from the pinpad in the order specified by the protocol and message type.</param>
-		/// <see cref="PollRequest"/>
 		public PurchaseResponse(IList<string> fieldValues) : base(fieldValues) { }
 
-		/// <summary>
-		/// Returns "PUR".
-		/// </summary>
 		public override string MessageType { get { return POSConstants.MessageType_Purchase; } }
 		
 	}
--- POSSimulator/VoidResponse.cs
+++ POSSimulator/VoidResponse.cs
@@ -6,18 +6,8 @@ namespace POSSimulator
 {
     public sealed class VoidResponse : POSResponseBase
 	{
-
-		/// <summary>
-		/// Constructs a new message instance from the list of pre-decoded string values received from the pinpad.
-		/// </summary>
-		/// <param name="fieldValues">The list of values returned from the pinpad in the order specified by the protocol and message type.</param>
-		/// <see cref="PollRequest"/>
 		public VoidResponse(IList<string> fieldValues) : base(fieldValues) { }
 
-		/// <summary>
-		/// Returns "REF".
-		/// </summary>
 		public override string MessageType { get { return POSConstants.MessageType_Refund; } }
-		
 	}
 }
