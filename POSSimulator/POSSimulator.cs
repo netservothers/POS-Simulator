7b1024c3b29c16c912ab5eb9eb62f38a2ff9662c
Aruna.V <aruna.v@smnetserv.com>
2021-01-07 22:04:41 +0530

    POSSimulator Console

--- POSSimulator/POSSimulator.cs
+++ POSSimulator/POSSimulator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using POS_KIOSK_Payment_Plugin;
 
 namespace POSSimulator
@@ -22,16 +24,69 @@ namespace POSSimulator
             
         }
 
-        #region Public Methods
+        #region Public Methods 
 
         public string ProcessReadRequest(string input)
-        { 
-            return string.Empty;
+        {
+            byte[] inputString = HexStringToBytes(input);
+
+            //string readData = Encoding.Default.GetString(inputString);
+
+            //string readASCIIData = Encoding.ASCII.GetString(inputString);
+
+            //string readUnicodeData = Encoding.Unicode.GetString(inputString);
+
+            //string readUTF32Data = Encoding.UTF32.GetString(inputString);
+
+            //string readUTF72Data = Encoding.UTF7.GetString(inputString);
+
+            //string readUTF8Data = Encoding.UTF8.GetString(inputString);
+
+            //string readBigEndianUnicodeData = Encoding.BigEndianUnicode.GetString(inputString);
+
+            return HextoString(input);
         }
 
         #endregion
 
         #region Private Methods
+
+        private static byte[] HexStringToBytes(string hexString)
+        {
+            if (hexString == null)
+                throw new ArgumentNullException("hexString");
+            if (hexString.Length % 2 != 0)
+                throw new ArgumentException("hexString must have an even length", "hexString");
+            var bytes = new byte[hexString.Length / 2];
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                string currentHex = hexString.Substring(i * 2, 2);
+                bytes[i] = Convert.ToByte(currentHex, 16);
+            }
+            return bytes;
+        }
+
+        private static string HextoString(string hexString)
+        {
+            string stringValue = "";
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < hexString.Length; i += 2)
+            {
+                var hexChar = hexString.Substring(i, 2);
+                sb.Append((char)Convert.ToByte(hexChar, 16));
+            }
+            return sb.ToString();
+
+
+            for (int i = 0; i < hexString.Length / 2; i++)
+            {
+                string hexChar = hexString.Substring(i * 2, 2);
+                int hexValue = Convert.ToInt32(hexChar, 16);
+                stringValue += Char.ConvertFromUtf32(hexValue);
+            }
+            return stringValue;
+        }
 
         #endregion
     }
---
+++ POSSimulatorConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POSSimulator;
+
+namespace POSSimulatorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            POSSimulator.POSSimulator simulatorObj = new POSSimulator.POSSimulator();
+
+            string value = simulatorObj.ProcessReadRequest("02003536303030303030303030313032303030301C343000123030303030303031313030301C0315");
+            Console.WriteLine("Input String:", value);
+            Console.ReadLine();
+        }
+    }
+}
